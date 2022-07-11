b9ffa3e19b89c1dc5bf0ae3d910b68c330aa9f9f
Connor Nickol <cnickol26@gmail.com>
2022-07-11 10:35:46 -0400

    Setup File

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Demo',
+      version='0.1',
+      description='A pointless demonstration package',
+      url='https://github.com/cnickol26/M14_Exercises',
+      author='Connor and Josh',
+      author_email='can2hr@virginia.edu',
+      license='MIT',
+      packages=['demo'])
