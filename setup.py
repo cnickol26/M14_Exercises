a6bbffe3289794110089f5508c095177dc7f337d
Josh Gen <64161160+jdg9vr@users.noreply.github.com>
2022-07-11 10:44:29 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='Demo',
+setup(name='M14_Exercises',
       version='0.1',
-      description='A pointless demonstration package',
+      description='A mood discovering package',
       url='https://github.com/cnickol26/M14_Exercises',
       author='Connor and Josh',
       author_email='can2hr@virginia.edu',
       license='MIT',
-      packages=['demo'])
+      packages=['M14_Exercises'])
